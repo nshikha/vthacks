a2967fe4c0a3169bbdd1238dece24a086b09fafd
Steve McQuaid <steve@stevemcquaid.com>
2014-04-20 03:15:09 -0400

    random shit?halfway thru the color path bug

--- util.js
+++ util.js
@@ -1,2 +1,6 @@
 var _id_ = 0;
 exports.getUID = function() { return (_id_++).toString(); };
+
+// light blue // light green // light purple // orange // light red/pink // yellow //  brown
+exports.COLORS = function() { return ['#007fff', '#3fb618', '#9954bb', '#ff7518', '#ff0039', '#debb27', '#ad1d28', '#7a5230'];};
+exports.ALPHABET = function() { return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];};
